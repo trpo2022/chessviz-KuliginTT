b1491055258e3d17e02f7afaaf93bae79a0d3794
Kuligin_F <fil.kuligin@mail.ru>
2022-05-24 13:06:12 +0700

    Fixed bugs

---
+++ obj/src/chessviz/chess_move.d
@@ -0,0 +1,2 @@
+obj/src/chessviz/chess_move.o: srс/chessviz/chess_move.c \
+ srс/chessviz/chess_move.h
---
+++ obj/src/chessviz/main.d
@@ -0,0 +1,2 @@
+obj/src/chessviz/main.o: src/chessviz/main.c \
+ src/chessviz/../libchessviz/board.h src/chessviz/chess_move.h
